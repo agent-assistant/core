af2216c21269c9c6ec260210dd325997977b2331
Blake <bleonard252@rocketmail.com>
2020-10-02 21:36:41 -0400

    Clap for suggestions! (Not... really implemented yet.)

--- main.rs
+++ main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
     println!("RUSTACEAN!!");
+    let matches = App::new("AgentCore")
+    .arg(
+        Arg::new("suggestions")
+            .about("Returns suggestions for the string.")
+            .short('s')
+            .long("suggestions")
+    )
+    .after_help("Agent Core is meant to be embedded into your apps!")
+    .get_matches();
+    if matches.is_present("suggestions") {
+        println!("You've enabled suggestions!")
+    }
 }
