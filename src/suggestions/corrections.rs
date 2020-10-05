1c8e236847d75b60d9ece06819ba486c2dde8bb3
Blake <bleonard252@rocketmail.com>
2020-10-05 18:40:35 -0400

    Eventually sort things out

--- src/suggestions/corrections.rs
+++ src/suggestions/corrections.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashMap;
+use std::convert::TryInto;
 use json;
 use strsim::sorensen_dice;
 
@@ -41,27 +42,32 @@ fn _corrections(input: &str, dict: Vec<String>, limit: i32) -> Vec<json::JsonVal
         let diff = sorensen_dice(&input.to_lowercase(), &item);
         if diff > 0.5 {result_set.insert(item, diff);}
     }
-    todo!("TODO: Sort results better");
-    //let results = result_set.iter().partial_cmp(|a: (&str, &f64), b: (&str, &f64)| a.1.partial_cmp(&b.1).iter()).unwrap_or((&String::default(), &0.0));
+    //todo!("TODO: Sort results better");
+    //let results = result_set.iter().cmp(|a: (&str, &f64), b: (&str, &f64)| a.1.partial_cmp(&b.1)).unwrap_or((&String::default(), &0.0));
+    let _results = result_set.iter();
+    let mut results: Vec<(String, &f64)> = Vec::new();
+    results.append(_results.collect() as &mut Vec<(String, &f64)>);
+    results.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    results.truncate(limit.try_into().unwrap());
     //let results = result_set.iter().b(|(a, b)| a.1.partial_cmp(&b.1).unwrap()).unwrap_or((&String::default(), &0.0));
-    // let mut objs: Vec<json::JsonValue> = vec![];
-    // for result in results {
-    //     json::object!{
-    //         "type": "correction",
-    //         "text": "",
-    //         "correct": ""
-    //     }
-    // }
-    // return objs
+    let mut objs: Vec<json::JsonValue> = vec![];
+    for result in results {
+        json::object!{
+            "type": "correction",
+            "text": result.0,
+            "replace_with": result.0
+        }
+    }
+    return objs
 }
 
 /// Return JSON formatted corrections with a dictionary.
 #[allow(unused)]
-pub fn corrections_dict(input: &str, dict: Vec<String>) -> json::JsonValue {
+pub fn corrections_dict(input: &str, dict: Vec<String>) -> Vec<json::JsonValue> {
     return _corrections(input, dict, 7)
 }
 /// Return JSON formatted corrections with a dictionary and custom limit.
 #[allow(unused)]
-pub fn corrections_dict_limited(input: &str, dict: Vec<String>, limit: i32) -> json::JsonValue {
+pub fn corrections_dict_limited(input: &str, dict: Vec<String>, limit: i32) -> Vec<json::JsonValue> {
     return _corrections(input, dict, limit)
 }
