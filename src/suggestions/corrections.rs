dcf2b90c6d7f8081017087d903fc5505da297f38
Blake <bleonard252@rocketmail.com>
2020-10-05 20:16:27 -0400

    [sugg] Do not require wordlist/dir
    If you forget -w or -W, it'll give you no corrections, instead of
    panicking.
    
    Also tweaked the settings for string similarities on corrections.

--- src/suggestions/corrections.rs
+++ src/suggestions/corrections.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::convert::TryInto;
 use json;
-use strsim::sorensen_dice;
 
 #[allow(dead_code)]
 pub fn load_dictionary(dir: bool, path: &str) -> Vec<String> {
@@ -22,11 +21,13 @@ pub fn load_dictionary(dir: bool, path: &str) -> Vec<String> {
     else {filelist.insert(filelist.len(), path.to_owned())}
     let mut wordlist: Vec<&str> = vec![];
     let content: &mut String = &mut String::default();
-    for file in filelist {
-        let file = File::open(file);
-        let mut fc = file.unwrap();
-        content.insert_str(content.len(), "\n");
-        fc.read_to_string(content).unwrap_or_default();
+    if filelist.len() > 0 && filelist.first().unwrap_or(&"".to_owned()) != "" {
+        for file in filelist {
+            let file = File::open(file);
+            let mut fc = file.unwrap();
+            content.insert_str(content.len(), "\n");
+            fc.read_to_string(content).unwrap_or_default();
+        }
     }
     wordlist.append(&mut content.split('\n').collect::<Vec<_>>());
     let mut nwl: Vec<String> = vec![];
@@ -39,8 +40,8 @@ fn _corrections(input: &str, dict: Vec<String>, limit: i32) -> Vec<json::JsonVal
     //let mut result_set: HashMap<String, f64> = HashMap::new();
     let mut result_set: Vec<(String, f64)> = vec![];
     for item in dict {
-        let diff = sorensen_dice(&input.to_lowercase(), &item);
-        if diff > 0.5 {result_set.insert(result_set.len(), (item, diff));}
+        let diff = strsim::sorensen_dice(&input.to_lowercase(), &item);
+        if diff > 0.6 {result_set.insert(result_set.len(), (item, diff));}
     }
     //todo!("TODO: Sort results better");
     //let results = result_set.iter().cmp(|a: (&str, &f64), b: (&str, &f64)| a.1.partial_cmp(&b.1)).unwrap_or((&String::default(), &0.0));
